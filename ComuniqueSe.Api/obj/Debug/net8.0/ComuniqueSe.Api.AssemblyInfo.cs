b270ebcfd91546b3480a95e38f0cf0b8a53de738
Felipe <ffsumida@gmail.com>
2026-07-20 16:28:30 -0300

    Testando todas as funções do sistema

--- ComuniqueSe.Api/obj/Debug/net8.0/ComuniqueSe.Api.AssemblyInfo.cs
+++ ComuniqueSe.Api/obj/Debug/net8.0/ComuniqueSe.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComuniqueSe.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85cf06953ead928b9a71ebd60fa9385fd8794493")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67c1bce9e708690040d23739414b54da22143e85")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComuniqueSe.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComuniqueSe.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
